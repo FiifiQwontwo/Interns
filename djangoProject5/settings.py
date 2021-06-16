d69e55bedfb6e6b9e33e2e9be3a2610eabf07b07
fiifiQwontwo <quophi555@gmail.com>
2021-06-16 13:00:42 -0700

    asa

--- djangoProject5/settings.py
+++ djangoProject5/settings.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Interns.apps.InternsConfig',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
